451b50122098b7b615bdd60a15da4d1d3b0f69ae
GollyTicker <golly.ticker@gmail.com>
2013-11-29 14:34:20 +0100

    maybo compilable dummy?

--- Aufgabe3/vmaccess.c
+++ Aufgabe3/vmaccess.c
@@ -5,4 +5,22 @@
  * This file gives the vmappl.c access to the virtual memory.
  */
 
+
 // TODO: connect to shared memory
+
+// dummy functionality
+
+int memory[550];
+
+
+int vmem_read(int address){
+    return memory[address];
+}
+
+
+void vmem_write(int address, int data){
+    memory[address]=data;
+}
+
+
+
