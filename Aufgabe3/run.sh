366b7ca20d1469a3089b239ee1db6895aa2d29d9
sacry- <nitsche25@gmail.com>
2013-12-01 09:46:59 -0500

    finished

--- Aufgabe3/run.sh
+++ Aufgabe3/run.sh
@@ -2,20 +2,6 @@
 
 make clean
 make all
-
-manage_map()
-{
-  if [ "$1" == "1" ] ; then
-    ./mmanage &
-    ./vmappl
-    killall -9 mmanage
-  fi
-}
-
-if [ $# -gt 0 ] ; then
-  manage_map
-else
-  ./mmanage
-fi
+./mmanage
 
 echo "process finished!"
